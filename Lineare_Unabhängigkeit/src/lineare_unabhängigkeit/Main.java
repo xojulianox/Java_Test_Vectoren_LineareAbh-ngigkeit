74a847a8209b04ed759f460ca2820c994e1f7bad
xojulianox <43339474+xojulianox@users.noreply.github.com>
2018-11-12 18:15:19 +0100

    Add files via upload

---
+++ Lineare_Unabhängigkeit/src/lineare_unabhängigkeit/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lineare_unabhängigkeit;
+
+/**
+ *
+ * @author xi97giq
+ */
+public class Main {
+    
+    
+    public static void main (String[]args){
+       Lineare_unabhängigkeit_form LuForm = new Lineare_unabhängigkeit_form();
+       LuForm.setVisible(true);
+    }
+    
+}
